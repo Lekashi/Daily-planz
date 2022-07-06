d04c715106b10e8e6e88e757b6314b0805baac3b
Nick Pisano <pisano.nicholas@yahoo.com>
2022-07-05 22:20:33 -0700

    Added in Buttonfunction for localstorage

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,14 @@
 var currentTime = moment().format("hh:mm:ss a")
+var timeBlockEl = $(".time-block")
+timeBlockEl.each(function (i, block) {
+    var blockID = $[(block).attr("id")]
+    var localText = localStorage.getItem(parseInt(blockID)) || "";
+    $(block).children("textarea").val(localText)
+    console.log(localText)
+    console.log(blockID)
+}) 
+
+
 
 function currentDate() {
     var day = moment().format("dddd MMMM, Do");
@@ -11,13 +21,12 @@ setInterval(currentDate, 1000)
 
 var saveButton = $(".saveBtn")
 
-saveButton.on('click' , function () {
-    timePastFuture();
-console.log($(this).siblings(".description").val())
-    })
 
-    // function timePastFuture () {
-    //     if (currentTime >= 
-    // }
-
-    // var
+$('.time-block').on('click', 'button', saveToLocal)
+function saveToLocal(event) {
+var element = event.target;
+var hour = $(element).parent('time-block').attr('id');
+var task = $(element).sibling("textarea").val();
+console.log(element)
+localStorage    
+}
